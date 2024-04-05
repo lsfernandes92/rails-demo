77f9eec03a1d5af18c31222851d6704b91581065
lsfernandes92 <lsfernandes92@gmail.com>
2024-04-05 14:49:45 -0300

    Fix some tests

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,5 @@ class Post < ApplicationRecord
   
   has_rich_text :content
   
-  has_many :comments
+  has_many :comments, dependent: :destroy
 end
--- spec/mailers/comments_spec.rb
+++ spec/mailers/comments_spec.rb
@@ -2,16 +2,23 @@ require "rails_helper"
 
 RSpec.describe CommentsMailer, type: :mailer do
   describe "submitted" do
-    let(:mail) { CommentsMailer.submitted }
+    before do
+      post = Post.create!(title: "Foo title", content: "Foo post content")
+      post.comments.create!(content: "Foo comment")
+    end
+    
+    let(:first_post) { Post.first }
+    let(:first_comment) { Comment.first }
+    let(:mail) { CommentsMailer.submitted(first_comment) }
 
     it "renders the headers" do
-      expect(mail.subject).to eq("Submitted")
-      expect(mail.to).to eq(["to@example.org"])
+      expect(mail.subject).to eq("Your blog post have a new comment!")
+      expect(mail.to).to eq(["blog-owner@example.org"])
       expect(mail.from).to eq(["from@example.com"])
     end
 
     it "renders the body" do
-      expect(mail.body.encoded).to match("Hi")
+      expect(mail.body.encoded).to include("You got a new comment on \"Foo title\" post!")
     end
   end
 
